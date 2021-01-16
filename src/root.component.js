3987f9e6184b0b55ea363d5f309ea92612b7e73f
Admir Cosic <222551@skane.se>
2021-01-16 13:22:59 +0100

    react-router implemented

--- src/root.component.js
+++ src/root.component.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { BrowserRouter, Link } from "react-router-dom";
 import "./root.component.css";
 
 export default function Root() {
   return (
     <BrowserRouter>
       <nav className="nav">
-        <Link to="/page1" className="link">
-          Page 1
+        <Link to="/app1" className="link">
+          Micro frontend app 1
         </Link>
-        <Link to="/page2" className="link">
-          Page 2
+        <Link to="/app2" className="link">
+          Micro frontend app 2
         </Link>
       </nav>
     </BrowserRouter>
